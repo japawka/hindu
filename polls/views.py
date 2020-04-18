8313f9d945db0f0418f99a1d63173566b7a21e53
japawka <japawka@poczta.onet.pl>
2020-04-18 13:37:51 +0200

    finasl

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from django.urls import reverse
 from .models import Choice, Question
 from django.views import generic
-
+from django.utils import timezone
 # def index(request):
 # 	latest_question_list = Question.objects.order_by('-pub_date')[:4]
 # 	context = {
@@ -17,7 +17,9 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('pub_date')[:5]
 
 
 
